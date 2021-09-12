77dc60ae9b0d3242cf052062ed52be4600833e84
Khushi Purwar <khushipurwar4@gmail.com>
2021-09-12 11:25:07 +0530

    intial commit

--- server/db.js
+++ server/db.js
@@ -8,8 +8,10 @@ module.exports = async () => {
             useUnifiedTopology: true,
             useFindAndModify: false,
         };
+        const uri = process.env.MONGO_URI;
         await mongoose.connect(
-            "mongodb://localhost/todo-app",
+            // "mongodb://localhost/todo-app",
+            uri,
             connectionParams
         );
         console.log("Connected to database.");
