59ee72989de3d1e50a2954804d5a74ac684da65f
Khushi Purwar <khushipurwar4@gmail.com>
2021-09-12 11:30:18 +0530

    intial commit

--- server/db.js
+++ server/db.js
@@ -8,7 +8,7 @@ module.exports = async () => {
             useUnifiedTopology: true,
             useFindAndModify: false,
         };
-        const uri = "mongodb+srv://khushi14:khushipurwar@todoapp.vrinj.mongodb.net/todo-app?retryWrites=true&w=majority";
+        const uri = process.env.MONGO_URI;
         await mongoose.connect(
             // "mongodb://localhost/todo-app",
             uri,
